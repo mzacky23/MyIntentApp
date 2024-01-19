dab24d360d216ec941c32e493c465cec697eb886
Muhammad Zacky Abdillah <muhammadzackya86@gmail.com>
2024-01-19 14:37:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myintentapp
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
+
+        val btnMoveToPersonalData: Button = findViewById(R.id.btn_move_to_personal_data)
+        btnMoveToPersonalData.setOnClickListener(this
+        )
+    }
+
+
+    override fun onClick(v: View) {
+            when (v.id) {
+                R.id.btn_move_activity -> {
+                    val moveIntent = Intent(
+                        this@MainActivity,
+                        MoveActivity::class.java)
+                    startActivity(moveIntent)
+                }
+                R.id.btn_move_activity_data ->{
+                    val moveWithDataIntent = Intent(this@MainActivity,
+                        MoveWithDataActivity::class.java)
+
+                    moveWithDataIntent.putExtra(
+                        MoveWithDataActivity.EXTRA_NAME,
+                        "Muhammad Zacky Abdillah")
+
+                    moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 18)
+                    startActivity(moveWithDataIntent)
+                }
+                R.id.btn_dial_number ->{
+                    val phoneNumber = "+6289509480382"
+                    val dialPhoneIntent = Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:$phoneNumber"))
+                    startActivity(dialPhoneIntent)
+                }
+                R.id.btn_move_to_personal_data -> {
+                    val moveToPersonalDataIntent = Intent(this@MainActivity,
+                        MoveToPersonalData::class.java)
+
+                    moveToPersonalDataIntent.putExtra(MoveToPersonalData.EXTRA_TITLE,
+                        "BIODATA")
+                    moveToPersonalDataIntent.putExtra(MoveToPersonalData.EXTRA_NAME,
+                        "Muhammad Zacky Abdillah")
+                    moveToPersonalDataIntent.putExtra(MoveToPersonalData.EXTRA_AGE,
+                        18)
+                    moveToPersonalDataIntent.putExtra(MoveToPersonalData.EXTRA_ADDRESS,
+                        "Jl.Sememi Jaya VI No.26")
+                    moveToPersonalDataIntent.putExtra(MoveToPersonalData.EXTRA_IDENTITY,
+                        "XI RPL 3 (30)")
+
+                    startActivity(moveToPersonalDataIntent)
+                }
+            }
+    }
+}
